4c38a6f4dd8df5081f55db3ad85e668466724772
Chris Campo <ccampo133@users.noreply.github.com>
2018-12-11 12:16:37 -0500

    feat: add version controller

--- src/main/java/me/ccampo/versiondemo/VersionDemoApplication.java
+++ src/main/java/me/ccampo/versiondemo/VersionDemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class VersionDemoApplication {
 
@@ -16,8 +18,18 @@ public class VersionDemoApplication {
     public static class VersionController {
 
         @GetMapping("/version")
-        public String version() {
-            return this.getClass().getPackage().getImplementationVersion();
+        public Version version() {
+            return new Version();
+        }
+    }
+
+    public static class Version {
+
+        public final String version;
+
+        public Version() {
+            this.version = Optional.ofNullable(this.getClass().getPackage().getImplementationVersion())
+                            .orElse("undefined");
         }
     }
 }
