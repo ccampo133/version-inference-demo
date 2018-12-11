dbfd8ad0116009dc769bf02623a703664c6a96bf
Chris Campo <ccampo133@users.noreply.github.com>
2018-12-11 13:51:37 -0500

    feat: minor enhancements

--- src/test/java/me/ccampo/versiondemo/VersionDemoApplicationTests.java
+++ src/test/java/me/ccampo/versiondemo/VersionDemoApplicationTests.java
@@ -2,15 +2,32 @@ package me.ccampo.versiondemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class VersionDemoApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() {
+        // Do nothing. If there is any exception during startup, this test fails.
     }
 
+    @Test
+    public void testVersionEndpoint() {
+        final ResponseEntity<VersionDemoApplication.Version> response =
+                restTemplate.getForEntity("/version", VersionDemoApplication.Version.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new VersionDemoApplication.Version());
+    }
 }
